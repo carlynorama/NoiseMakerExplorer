16868ab3ac0f7d799f616203620932623ce92483
carlynorama <carlynorama@gmail.com>
2023-03-12 13:51:14 -0700

    Initial Commit

---
+++ NoiseMakerExplorer/NoiseMakerExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoiseMakerExplorerApp.swift
+//  NoiseMakerExplorer
+//
+//  Created by Carlyn Maw on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NoiseMakerExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
