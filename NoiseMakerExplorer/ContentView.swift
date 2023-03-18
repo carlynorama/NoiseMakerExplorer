40d30d9c6987ea09f35e67640db8f61bf0739b4d
carlynorama <carlynorama@gmail.com>
2023-03-18 08:59:02 -0700

    hello noise

--- NoiseMakerExplorer/ContentView.swift
+++ NoiseMakerExplorer/ContentView.swift
@@ -1,21 +1,37 @@
 //
 //  ContentView.swift
-//  NoiseMakerExplorer
+//  NoiseExplorer
 //
-//  Created by Carlyn Maw on 3/12/23.
+//  Created by Carlyn Maw on 3/10/23.
 //
 
 import SwiftUI
+import Charts
+import NoiseMaker
 
 struct ContentView: View {
+    let myVals = NoiseMaker.blurredRandomArray(count: 16, octaves: 8, scalingBias: 1)
+    let my2DVals = NoiseMaker.blurredRandomBuffer2D(width: 16, height: 16, octaves: 8, scalingBias: 1)
+    let testInt = 123
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Chart(Array(myVals.enumerated()), id: \.offset) { item in
+                LineMark(x: .value("index", item.offset), y: .value("value", item.element))
+                
+            }
+
+            Button("Print 2d") {
+                print2D()
+            }
         }
         .padding()
+    }
+    
+    func print2D() {
+        my2DVals.asciiBWPrint()
+        let noise = NoiseMaker.perlinWrapper(x: Double.random(in: 0...1), y: Double.random(in: 0...1))
+        print("\nperlin: \(noise)")
     }
 }
 
